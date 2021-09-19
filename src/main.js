accb30a92b8190942ec63c9601b0a29ed201e66d
NataliaTepluhina <tarya.se@gmail.com>
2021-09-19 14:08:29 +0200

    Reverted changes to start

--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter } from 'vue-router'
-import routes from './routes'
-import { createWebHashHistory } from 'vue-router/dist/vue-router.cjs'
 
-const router = createRouter({
-  routes,
-  history: createWebHashHistory(),
-})
-
-const app = createApp(App)
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
--- src/views/ItemDetails.vue
+++
@@ -1,1 +0,0 @@
-<template>Items placeholder</template>
